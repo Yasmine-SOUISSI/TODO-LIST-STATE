0b35e72051029ffe274d8c15409f377d16b130f0
Yasmine <yasmine.souissi@esprit.tn>
2022-03-28 10:47:10 +0200

    app state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import AppCount from "./Components/Counter/AppCount";
+import AddTask from "./Components/Todo/AddTask";
+import TodoList from "./Components/Todo/TodoList";
+
+export default class App extends Component {
+    state = {
+        tasks: [
+            { task: "Do Homework", done: false, id: 1 },
+            { task: "Eat", done: false, id: 2 },
+            { task: "Sleep", done: false, id: 3 },
+        ],
+    };
+    //Add Task
+    addTask = (task) => {
+        const newTask = {
+            task,
+            done: false,
+            id: Math.random(),
+        };
+        this.setState({
+            tasks: [...this.state.tasks, newTask],
+        });
+    };
+    //Delete Task
+    deleteTask = (id) => {
+        this.setState({
+            tasks: this.state.tasks.filter((el) => el.id != id),
+        });
+    };
+
+    //Done/Undone Task
+    handleDone = (id) => {
+        this.setState({
+            tasks: this.state.tasks.map((el) =>
+                el.id == id ? { ...el, done: !el.done } : el
+            ),
+        });
+    };
+    render() {
+        return (
+            <div>
+                <TodoList
+                    tasks={this.state.tasks}
+                    handleDone={this.handleDone}
+                    deleteTask={this.deleteTask}
+                />
+                <AddTask addTask={this.addTask} />
+            </div>
+        );
+    }
 }
 
-export default App;
+//Function Component
+// const App()=>{
+//   return (
+//     <div>App</div>
+//   )
+// }
